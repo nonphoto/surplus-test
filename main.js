6c854787a22bfe0c069d5820d7b17941efa563ac
Jonas Luebbers <jonaskluebbers@gmail.com>
2020-07-28 21:31:21 -0400

    Create perf test

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
 import SArray from "s-array";
 import * as Surplus from "surplus";
+import S from "s-js";
 
-const list = SArray([0, 1, 2, 3]);
+const range = (n) => [...Array(n).keys()];
+
+let t = S.data(0),
+  ts = S((ts) => ((ts[1] = ts[0]), (ts[0] = t()), ts), [0, 0]),
+  dt = S(() => ts()[0] - ts()[1]),
+  loop = (_t) => (t(_t), requestAnimationFrame(loop));
+
 const a = (
-  <div>
-    {list.map((n) => (
-      <h1 style={{ background: "red" }}>{n}</h1>
+  <div style={{ display: "flex", flexWrap: "wrap", overflow: "hidden" }}>
+    {range(100).map((n) => (
+      <div
+        style={{
+          background: "gray",
+          borderRadius: "100vw",
+          width: "50px",
+          height: "50px",
+          transform: `translate(${Math.cos(t() * 0.01 + n) * 10}px, ${
+            Math.sin(t() * 0.01 + n) * 10
+          }px)`,
+        }}
+      ></div>
     ))}
   </div>
 );
 
 document.body.appendChild(a);
+
+loop();
